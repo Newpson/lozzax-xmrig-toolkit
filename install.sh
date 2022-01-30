3f36a38a59c42c21f2d121f6b60b32ff0afa7879
Newpson <dk.newpson@gmail.com>
2022-01-30 18:42:04 +0300

    now the miner is downloaded from the developer's repo

--- install.sh
+++ install.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
 INST_DIR=/opt/lozzax-xmrig
+LATEST=$(curl --silent "https://api.github.com/repos/lozzax-project/xmrig/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
 set -e
+wget https://github.com/lozzax-project/xmrig/releases/download/$LATEST/lozzax-xmrig.tar.gz
+tar xf lozzax-xmrig.tar.gz --strip-components 1
+rm -v lozzax-xmrig.tar.gz
 
 mkdir -pv $INST_DIR
 cp -v xmrig miner.sh address $INST_DIR
